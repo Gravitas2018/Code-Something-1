b57b57c6ed2cdb4dca04a65d61dc0828302a99a8
Marko <tenjerma@gmail.com>
2019-01-20 19:48:50 -0800

    add axes

---
+++ TestConsoleApp/TestConsoleApp/Axe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Axe:Item
+{
+
+    public int dps;
+
+    public Axe(int _dps, string namePassed, string descriptionPassed): base(namePassed, descriptionPassed)
+    {
+        this.dps = _dps;
+    }
+
+}
--- TestConsoleApp/TestConsoleApp/BetterInventory.cs
+++ TestConsoleApp/TestConsoleApp/BetterInventory.cs
@@ -13,9 +13,13 @@ class BetterInventory
     Sword sword2 = new Sword(2, "Two Handed Sword", "Hold this shit with two hands");
     Sword sword3 = new Sword(3, "Long Sword", "This shit long");
 
+    Axe axe1 = new Axe(1, "One Handed Axe", "Hold this shit with one hand");
+    Axe axe2 = new Axe(2, "Two Handed Axe", "Hold this shit with two hands");
+    Axe axe3 = new Axe(3, "Kingslayer", "This shit kill kings");
 
 
-    Item[,] inventory = new Item[,] { { sword1, sword2, sword3 } };
+
+        Item[,] inventory = new Item[,] { { sword1, sword2, sword3 }, { axe1, axe2, axe3 } };
 
         Console.WriteLine("\nInventory:");
         foreach (Item item in inventory)
