f625094871e395eb3a9fa7c7ca2f711c1ed193de
Marko <tenjerma@gmail.com>
2019-01-20 17:46:12 -0800

    rm namespace print item

--- TestConsoleApp/TestConsoleApp/Character.cs
+++ TestConsoleApp/TestConsoleApp/Character.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeSomething
+
+public class Character
 {
-    public class Character
+    public string _name { get; set; }
+    public int _level { get; set; }
+    public Character(string name, int level)
     {
-        public string _name { get; set; }
-        public int _level { get; set; }
-        public Character(string name, int level)
-        {
-            _name = name;
-            _level = level;
-        }
+        _name = name;
+        _level = level;
     }
 }
+
