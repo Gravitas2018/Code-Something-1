68e05261715e419640c52e091e991352d61aba02
Gravitas <gravitas2018>
2019-01-20 18:40:25 -0800

    Added LinkNode

---
+++ TestConsoleApp/TestConsoleApp/LinkNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class LinkNode
+{
+    LinkNode next = null;
+    int data;
+
+    public LinkNode(int passedData)
+    {
+        data = passedData;
+    }
+
+    void appendToTail(int passedData)
+    {
+        LinkNode end = new LinkNode(passedData);
+        LinkNode n = this;
+        while (n.next != null)
+        {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+}
+
